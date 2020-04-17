ca0900824ad99a0ecea20b79f2dd7b1b25abed29
joaocarloshespanhol <63821867+joaocarloshespanhol@users.noreply.github.com>
2020-04-17 19:25:34 -0300

    Update site.py

--- site.py
+++ site.py
@@ -1,8 +1,45 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+<head>
+<header>
+  <h1>
+    UNIFRAN
+  </h1>
+</header>
+</head>
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-if __name__ == '__main__':
-    app.run() 
+<style>
+header{
+  height: 100vh;
+  width: 100%;
+  background-image: url(https://images.unsplash.com/photo-1462536943532-57a629f6cc60?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80);
+  background-position: center;
+  background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #f5f5f5;
+    background-attachment: fixed;
+}
+.cursos {
+  height: 100vh;
+  width: 100%;
+  background-image: url(https://images.unsplash.com/photo-1498079022511-d15614cb1c02?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80);
+  background-position: center;
+  background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #f5f5f5;
+    background-attachment: fixed;
+}
+
+
+
+
+
+</style>
+
+<div class='cursos'>
+  <h1>
+    MATRICULAS ABERTAS
+  </h1>
+</div>
